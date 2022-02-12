9227e41e876ba99b758c18c12c671d2ccbc77501
Komil Sobitov <64638545+komildeveloper@users.noreply.github.com>
2022-02-13 03:41:11 +0500

    logout btn added

--- js/app.js
+++ js/app.js
@@ -4,3 +4,8 @@ const localToken = window.localStorage.getItem('token')
 if (!localToken) {
 	window.location('login.html')
 }
+
+logout.addEventListener('click', () => {
+	window.localStorage.removeItem('token')
+	window.location.replace('login.html') // cityslicka
+})
