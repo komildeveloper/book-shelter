ca70f9803d49de648d273434e0dd08419332834b
Komil Sobitov <64638545+komildeveloper@users.noreply.github.com>
2022-02-13 03:33:13 +0500

    reqres toke

--- js/login.js
+++ js/login.js
@@ -1,1 +1,31 @@
 'use strict'
+
+const elForm = document.querySelector('.form')
+const elUserName = document.querySelector('.user__name')
+const elUserPassword = document.querySelector('.user__password')
+
+elForm.addEventListener('submit', e => {
+	e.preventDefault()
+
+	const inputUserName = elUserName.value
+	const inputUserPassword = elUserPassword.value
+
+	fetch('https://reqres.in/api/login', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({
+			email: inputUserName,
+			password: inputUserPassword
+		})
+	})
+		.then(request => request.json())
+		.then(data => {
+			if (data?.token) {
+				window.localStorage.setItem('token', data.token)
+
+				window.location.replace('index.html')
+			}
+		})
+})
